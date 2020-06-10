a2ac0249129e82ffd67b4b871a6a7c12e193d2bb
fran <franjimenezaguilera@gmail.com>
2020-06-10 21:47:15 +0200

    Add pca post request

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask 
+from flask import Flask, request, jsonify 
+from pca import pca
+from io import StringIO
+import os
 
 app = Flask(__name__)
 
 # Routes
-@app.route('/')
-def hello():
-    return 'Hello world!'
+@app.route('/pca', methods=['POST'])
+def pca_request():
+    
+    two_first_components_plot, components_and_features_plot, explained_variance_ratio = pca(request.files.get("csv"))
+    
+    return jsonify(
+        twoFirstComponentsPlot = two_first_components_plot,
+        componentsAndFeaturesPlot = components_and_features_plot, 
+        explainedVarianceRatio = explained_variance_ratio
+    )
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- pca.py
+++ pca.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import random
+import base64
+import io
 
 # TODO: find a new way to group the dataset
 CLUSTER_TYPES = ["Road", "Highway", "Dirty road"]
@@ -32,69 +34,88 @@ def generate_pc_columns_names(number):
 
     for i in range(number):
         res.append("pc"+str(i))
-    
+
     return res
 
 
-# load dataset
-df = pd.read_csv("live1.csv")
+def pca(csv_file):
 
-# TODO: Get the features from the first row in the csv
-# features
-features = ['ENGINE_RUN_TIME ()', 'ENGINE_RPM ()', 'VEHICLE_SPEED ()', 'THROTTLE ()', 'ENGINE_LOAD ()',
-            'COOLANT_TEMPERATURE ()', 'LONG_TERM_FUEL_TRIM_BANK_1 ()', 'SHORT_TERM_FUEL_TRIM_BANK_1 ()', 'INTAKE_MANIFOLD_PRESSURE ()',
-            'FUEL_TANK ()', 'ABSOLUTE_THROTTLE_B ()', 'PEDAL_D ()', 'PEDAL_E ()', 'COMMANDED_THROTTLE_ACTUATOR ()', 'FUEL_AIR_COMMANDED_EQUIV_RATIO ()',
-            'ABSOLUTE_BAROMETRIC_PRESSURE ()', 'RELATIVE_THROTTLE_POSITION ()', 'INTAKE_AIR_TEMP ()', 'TIMING_ADVANCE ()', 'CATALYST_TEMPERATURE_BANK1_SENSOR1 ()',
-            'CATALYST_TEMPERATURE_BANK1_SENSOR2 ()', 'CONTROL_MODULE_VOLTAGE ()', 'COMMANDED_EVAPORATIVE_PURGE ()', 'TIME_RUN_WITH_MIL_ON ()',
-            'TIME_SINCE_TROUBLE_CODES_CLEARED ()', 'DISTANCE_TRAVELED_WITH_MIL_ON ()', 'WARM_UPS_SINCE_CODES_CLEARED ()']
+    # Read file
+    df = pd.read_csv(csv_file)
+  
+    # Get features from the first row
+    features = df.columns.values
 
-# Remove features
-x = df.loc[:, features].values
+    # Remove features
+    x = df.loc[:, features].values
 
-# Standardizing (mean = 0 , variance = 1)
-x = StandardScaler().fit_transform(x)
+    # Standardizing (mean = 0 , variance = 1)
+    x = StandardScaler().fit_transform(x)
 
-# Create pca
-pca = PCA(n_components=NUMBER_COMPONENTS)
+    # Create pca
+    pca = PCA(n_components=NUMBER_COMPONENTS)
 
-# Create the principal components
-principal_components = pca.fit_transform(x)
+    # Create the principal components
+    principal_components = pca.fit_transform(x)
 
-# Create columns labels for each component
-pc_colums_names = generate_pc_columns_names(NUMBER_COMPONENTS)
+    # TODO: Add chart to Dataset entity 
+    # Number of components we should choose
+    # pca = PCA().fit(x)
+    # plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    # plt.xlabel('number of components')
+    # plt.ylabel('cumulative explained variance')
+    #plt.savefig("cumulative_explained_variance.png", bbox_inches='tight')
 
-principal_components_df = pd.DataFrame(
-    data=principal_components, columns=pc_colums_names)
+    # Create columns labels for each component
+    pc_colums_names = generate_pc_columns_names(NUMBER_COMPONENTS)
+
+    principal_components_df = pd.DataFrame(
+        data=principal_components, columns=pc_colums_names)
+
+    # Add the cluster column
+    cluster_column = get_cluster_column(len(df.index)-1)
+    df_complete = pd.concat(
+        [principal_components_df, pd.Series(cluster_column)], axis=1)
+    df_complete.rename(columns={0: 'cluster'}, inplace=True)
+
+    # Plot two first compontens
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('Principal component 1', fontsize=16)
+    ax.set_ylabel('Principal component 2', fontsize=16)
+    ax.set_title('PCA', fontsize=22)
+    targets = CLUSTER_TYPES
+    colors = ['r', 'g', 'b']
+    for target, color in zip(targets, colors):
+        indicesToKeep = df_complete['cluster'] == target
+        ax.scatter(df_complete.loc[indicesToKeep, 'pc1'],
+                   df_complete.loc[indicesToKeep, 'pc2'], c=color, s=50)
+    ax.legend(targets)
+    ax.grid()
+    #plt.savefig("two_first_componets_plot.png")
+
+    # Create Base 64 string of the plot
+    pic_IObytes = io.BytesIO()
+    plt.savefig(pic_IObytes,  format='png')
+    pic_IObytes.seek(0)
+    two_first_components_plot = base64.b64encode(pic_IObytes.read())
+
+    # Print the amount of data that holds the components
+    explained_variance_ratio = pca.explained_variance_ratio_
+
+    # Create a plot for the porcetage of participation of each feature in each component
+    plt.matshow(pca.components_, cmap='magma')
+    plt.yticks([0, 1, 2], pc_colums_names, fontsize=10)
+    plt.colorbar()
+    plt.xticks(range(len(features)), features, rotation=90, ha='right')
+    #plt.savefig("pca_and_features_participation.png", bbox_inches='tight')
+
+    # Create Base 64 string of the plot
+    pic_IObytes = io.BytesIO()
+    plt.savefig(pic_IObytes,  format='png', bbox_inches='tight')
+    pic_IObytes.seek(0)
+    components_and_features_plot = base64.b64encode(pic_IObytes.read())
 
 
-# Add the cluster column
-cluster_column = get_cluster_column(len(df.index)-1)
-df_complete = pd.concat(
-    [principal_components_df, pd.Series(cluster_column)], axis=1)
-df_complete.rename(columns={0: 'cluster'}, inplace=True)
 
-# Plot two first compontens
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(1, 1, 1)
-ax.set_xlabel('Principal component 1', fontsize=16)
-ax.set_ylabel('Principal component 2', fontsize=16)
-ax.set_title('PCA', fontsize=22)
-targets = CLUSTER_TYPES
-colors = ['r', 'g', 'b']
-for target, color in zip(targets, colors):
-    indicesToKeep = df_complete['cluster'] == target
-    ax.scatter(df_complete.loc[indicesToKeep, 'pc1'],
-               df_complete.loc[indicesToKeep, 'pc2'], c=color, s=50)
-ax.legend(targets)
-ax.grid()
-plt.savefig("two_first_componets_plot.png")
-
-# Print the amount of data that holds the components
-print(pca.explained_variance_ratio_)
-
-# Create a plot for the porcetage of participation of each feature in each component
-plt.matshow(pca.components_, cmap='magma')
-plt.yticks([0, 1, 2], pc_colums_names, fontsize=10)
-plt.colorbar()
-plt.xticks(range(len(features)), features, rotation=90, ha='right')
-plt.savefig("pca_and_features_participation.png", bbox_inches='tight')
+    return two_first_components_plot.decode('ascii'), components_and_features_plot.decode('ascii'), pd.Series(explained_variance_ratio).to_json(orient='values')
